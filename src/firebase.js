c9e7a79d67a980eee6b80e5bc6772527f5376fed
nuryhan <nurmuhammet.muradov0309@gmail.com>
2021-12-20 17:51:37 +0500

    react-admin-dashboard

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0QU1GjxOtnk6DT0meBF8UxqO0o1lDmVM",
+  authDomain: "parlak-market.firebaseapp.com",
+  projectId: "parlak-market",
+  storageBucket: "parlak-market.appspot.com",
+  messagingSenderId: "355670560451",
+  appId: "1:355670560451:web:3dfc42738f185e4b841c76"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
